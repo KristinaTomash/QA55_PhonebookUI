b580f7af497cce554f76145c5dcae4c21b02d2a4
KristinaTomash <kristitomsh@gmail.com>
2025-04-28 13:33:20 +0300

    Add26.1

---
+++ src/main/java/com/phonebook/data/ContactData.java
@@ -0,0 +1,13 @@
+package com.phonebook.data;
+
+public class ContactData {
+    public static final String Name="Oliver";
+    public static final String LAST_Name="TWIST";
+    public static final String PHONE="01234567890";
+    public static final String EMAIL="Ol@gmail.com";
+    public static final String ADDRESS="Riahon";
+    public static final String DESCRIPTION="QA";
+
+
+
+}
